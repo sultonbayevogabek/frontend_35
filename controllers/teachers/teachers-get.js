e2e53b8e4908f58bb260bfb0ca866c37a811014b
sultonbayevogabek <sultonbayevogabek@gmail.com>
2021-09-29 16:31:00 +0500

    nimadir

--- controllers/teachers/teachers-get.js
+++ controllers/teachers/teachers-get.js
@@ -3,6 +3,7 @@ const readFile = require('../../modules/read-file')
 module.exports = async (req, res) => {
     res.render('teachers', {
         title: `O'quvchilar`,
-        teachers: await readFile('teachers.json')
+        teachers: await readFile('teachers.json'),
+        courses: await readFile('courses.json')
     })
 }
