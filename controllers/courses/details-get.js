20420bc6dc644eb8359c5142d1a22d162dabedb7
sultonbayevogabek <sultonbayevogabek@gmail.com>
2021-10-10 22:57:55 +0500

    fix bugs

--- controllers/courses/details-get.js
+++ controllers/courses/details-get.js
@@ -3,7 +3,6 @@ const readFile = require('../../modules/read-file')
 module.exports = async (req, res) => {
     let courses = await readFile('courses.json')
     let course = courses.find(c => c.id === req.params.id)
-    console.log(course)
     res.render('details', {
         title: `Kurs haqida`,
         courses,
